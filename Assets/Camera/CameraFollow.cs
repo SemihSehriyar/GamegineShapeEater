b61befbebf63dbb4089f5d39c17de5257f997628
Semih Sehriyar <semsehriyar@gmail.com>
2023-09-05 18:53:47 +0300

    Camera Follow

--- Assets/Camera/CameraFollow.cs
+++ Assets/Camera/CameraFollow.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private float _followSpeed = 2f;
+    private float _yOffSet = 1f;
+    private Transform _target;
     void Update()
     {
-        
+        Vector3 newPos = new Vector3(_target.position.x , _target.position.y + _yOffSet , -10f);
+        transform.position = Vector3.Slerp(transform.position,newPos, _followSpeed * Time.deltaTime);
     }
 }
