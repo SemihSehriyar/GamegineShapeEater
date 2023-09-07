a83b709f0e414645d1a03528b45e55eafe2049d8
Semih Sehriyar <semsehriyar@gmail.com>
2023-09-07 18:45:18 +0300

    Shape Spawner  updated

---
+++ Assets/Shapes/Shapes.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shapes : MonoBehaviour
+{
+    public EatableShapes eatables;
+}
+
+public enum EatableShapes 
+{
+    None,
+    Circle,
+    Square,
+    Hexagon,
+    NotEatable,
+}
